e34cff8ea5f8670d41bc7ce81d3eef7c4dc923e1
divyagupta2410 <70086712+divyagupta2410@users.noreply.github.com>
2020-08-23 13:26:19 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,17 @@
+
+#1.
+for i in range(2000,3201):
+    if(i%7==0 and i%5!=0):
+        print(i,end=',')
+
+#2.
+s=str(input())
+print(s[::-1])
+
+
+#3.
+from math import pi
+r=6
+v=4*pi*(r**3)/3
+print(v)
+
